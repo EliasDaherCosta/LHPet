1d34d71b58babdd6489e43fbef6936f635d640c9
Elias Daher Costa <eliasdahercosta@gmail.com>
2024-04-12 18:26:27 -0300

    Commit inicial

---
+++ Models/Cliente.cs
@@ -0,0 +1,9 @@
+namespace LHPet.Models;
+public class Cliente(int id, string nome, string cpf, string email, string paciente)
+{
+    public int Id { get; set; } = id;
+    public string? Nome { get; set; } = nome;
+    public string? Cpf { get; set; } = cpf;
+    public string? Email { get; set; } = email;
+    public string? Paciente { get; set; } = paciente;
+}
